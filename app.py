d00db6eadcfd3f7e24d4fffd3b6fe73f0a0e7cfb
WhyTologin <whytologin@gmail.com>
2026-02-17 15:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ if not USE_SUPABASE_AUTH:
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
     app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
     app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True').lower() == 'true'
+    app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False').lower() == 'true'
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     
